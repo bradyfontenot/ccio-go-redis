d9f1540f534b65f1983a0c9798e34c76f2f1c87d
bradyfontenot <bfont52@gmail.com>
2022-08-07 22:32:13 -0500

    response to ping

--- app/server.go
+++ app/server.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"os"
 )
+
+const pong = "+PONG\r\n"
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -15,9 +18,29 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	defer l.Close()
+
+	for {
+
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleConn(conn)
+
+	}
+}
+
+func handleConn(c net.Conn) {
+	for {
+		input := make([]byte, 2048)
+		_, err := c.Read(input)
+		if err != nil {
+			log.Fatal("could not read Conn")
+		}
+
+		c.Write([]byte(pong))
 	}
 }
